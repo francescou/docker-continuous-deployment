acb5f7a78bc06046b284b69c5633a2314832ed91
Francesco Uliana <francesco@uliana.it>
2015-10-25 16:31:30 +0100

    pylint

--- rest-count/main.py
+++ rest-count/main.py
@@ -2,24 +2,22 @@
 Flask webapp, registered as a consul client
 """
 
-from flask import Flask, jsonify, request
-from json import loads
+from flask import Flask, jsonify
 import logging
 import os
 from consulclient import register, deregister
-import os
-import redis
+from redis import StrictRedis
 
-PORT=8000
+PORT = 8000
 
 API_V1 = '/api/v1/'
 
 logging.basicConfig(level=logging.DEBUG)
 app = Flask(__name__, static_url_path='')
 
-register(PORT,  os.environ['TAGS'].split(',') if os.environ.has_key('TAGS') else None)
+register(PORT, os.environ['TAGS'].split(',') if os.environ.has_key('TAGS') else None)
 
-r = redis.StrictRedis(host='redis', port=6379, db=0)
+REDIS = StrictRedis(host='redis', port=6379, db=0)
 
 # REST endpoints
 @app.route(API_V1 + "health", methods=['GET'])
@@ -34,10 +32,10 @@ def index():
     """
     RESTful web service
     """
-    n = r.get('count')
-    n = int(n)+1 if n else 1
-    r.set('count', n)
-    return jsonify(version='1.0', kind=os.environ['TAGS'], count=n)
+    count = REDIS.get('count')
+    count = int(count) + 1 if count else 1
+    REDIS.set('count', count)
+    return jsonify(version='1.0', kind=os.environ['TAGS'], count=count)
 
 @app.errorhandler(Exception)
 def handle_generic_error(err):
@@ -48,7 +46,7 @@ def handle_generic_error(err):
 
 if __name__ == "__main__":
     try:
-        app.run(host='0.0.0.0', debug=True, threaded=True,port=PORT)
+        app.run(host='0.0.0.0', debug=True, threaded=True, port=PORT)
     finally:
         deregister()
         logging.warn('shutting down')
