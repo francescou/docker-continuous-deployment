c841f9d7baca416c56302fb920236714b118c67c
Francesco Uliana <francesco@uliana.it>
2015-10-25 16:40:09 +0100

    pylint

--- rest-ip/consulclient.py
+++ rest-ip/consulclient.py
@@ -1,21 +1,34 @@
+"""
+module to register and deregister services on consul
+"""
+
 import consul
 import socket
 import logging
 
-HOST='consul'
+HOST = 'consul'
 
 logging.basicConfig(level=logging.DEBUG)
-ip = socket.gethostbyname(socket.gethostname())
+IP = socket.gethostbyname(socket.gethostname())
 
-def register(port=8080,tags=[]):
-        logging.info('tags = ' + str(tags))
-	logging.info('application running on %s on port %i' % (ip, port))
-	cc = consul.Check.http(interval='5s', url='http://%s:%i/api/v1/health' % (ip, port))
-	outcome = consul.Consul(host=HOST).agent.service.register(tags=tags,name='rest-ip',address=ip,port=port,service_id='worker-%s' % ip,check=cc)
-	logging.info('consul registration ' + ('ok' if outcome else 'ko'))
-	return outcome
+def register(port=8080, tags=None):
+    """
+    register service to consul
+    """
+    if tags is None:
+        tags = []
+    logging.info('tags = ' + str(tags))
+    logging.info('application running on %s on port %i', IP, port)
+    check = consul.Check.http(interval='5s', url='http://%s:%i/api/v1/health' % (IP, port))
+    outcome = consul.Consul(host=HOST).agent.service.register(tags=tags, \
+      name='rest-ip', address=IP, port=port, service_id='worker-%s' % IP, check=check)
+    logging.info('consul registration ' + ('ok' if outcome else 'ko'))
+    return outcome
 
 
 def deregister():
-        logging.info('deregistering')
-        return consul.Consul(host=HOST).agent.service.deregister(service_id='worker-%s' % ip)
+    """
+    deregister service from consul
+    """
+    logging.info('deregistering')
+    return consul.Consul(host=HOST).agent.service.deregister(service_id='worker-%s' % IP)
--- rest-ip/main.py
+++ rest-ip/main.py
@@ -3,24 +3,22 @@ flask webapp, registered as a consul client
 """
 
 from flask import Flask, jsonify, request
-from json import loads
 import logging
 import os
 from consulclient import register, deregister
-import os
-import redis
+from redis import StrictRedis
 import datetime
 
-PORT=8000
+PORT = 8000
 
 API_V1 = '/api/v1/'
 
 logging.basicConfig(level=logging.DEBUG)
 app = Flask(__name__, static_url_path='')
 
-register(PORT,  os.environ['TAGS'].split(',') if os.environ.has_key('TAGS') else None)
+register(PORT, os.environ['TAGS'].split(',') if os.environ.has_key('TAGS') else None)
 
-r = redis.StrictRedis(host='redis', port=6379, db=0)
+REDIS = StrictRedis(host='redis', port=6379, db=0)
 
 # REST endpoints
 @app.route(API_V1 + "health", methods=['GET'])
@@ -35,11 +33,14 @@ def index():
     """
     RESTful webservice
     """
-    ip = r.get('ip')
-    time = r.get('time')
-    r.set('ip', request.remote_addr)
-    r.set('time', datetime.datetime.now().strftime("%H:%M:%S"))
-    return jsonify(version='1.0', kind=os.environ['TAGS'], ip=ip if ip else None, time=time if time else None)
+    last_ip = REDIS.get('ip')
+    time = REDIS.get('time')
+    REDIS.set('ip', request.remote_addr)
+    REDIS.set('time', datetime.datetime.now().strftime("%H:%M:%S"))
+    return jsonify(version='1.0', \
+        kind=os.environ['TAGS'], \
+        last_ip=last_ip if last_ip else None, \
+        time=time if time else None)
 
 @app.errorhandler(Exception)
 def handle_generic_error(err):
@@ -50,7 +51,7 @@ def handle_generic_error(err):
 
 if __name__ == "__main__":
     try:
-        app.run(host='0.0.0.0', debug=True, threaded=True,port=PORT)
+        app.run(host='0.0.0.0', debug=True, threaded=True, port=PORT)
     finally:
         deregister()
         logging.warn('shutting down')
