d2bc49100ddee43430b9bf002b12315d97d6da4c
Andrea Sosso <andrea.sosso@dnshosting.it>
2015-12-03 11:07:26 +0100

    consul-template updated to v0.11.1. Moving release to releases.hashicorp.com

--- application/nginx/nginx-start.sh
+++ application/nginx/nginx-start.sh
@@ -2,4 +2,4 @@
 
 service nginx start
 
-/opt/consul-template_0.10.0_linux_amd64/consul-template -consul consul:8500 -template '/opt/nginx.conf.tpl:/etc/nginx/nginx.conf:service nginx reload'
+/opt/consul-template -consul consul:8500 -template '/opt/nginx.conf.tpl:/etc/nginx/nginx.conf:service nginx reload'
