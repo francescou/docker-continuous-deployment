fbb968920c4263757a3ff7d7bc51c536c111fce1
Francesco Uliana <francesco@uliana.it>
2015-12-20 10:55:52 +0100

    fixed shutdown behaviour

--- rest-ip/app.js
+++ rest-ip/app.js
@@ -21,39 +21,36 @@ var y = _.filter(x[0], function (el) {
 
 var HOST = y[0].address;
 
-
 var TAGS = process.env.TAGS;
 var tags = TAGS.split(',');
 
-  consul.agent.service.register({
-    tags: tags,
-    name: 'rest-ip',
-    address: HOST,
-    port: PORT,
-    id: 'worker-' + HOST,
-    check: {
-      http: 'http://' + HOST + ':' + PORT + '/api/v1/health',
-      interval: '5s'
-    }
-  }, function (a, b, c) {
-    console.log('post service register')
-    console.log(a, b, c);
-  });
+consul.agent.service.register({
+  tags: tags,
+  name: 'rest-ip',
+  address: HOST,
+  port: PORT,
+  id: 'worker-' + HOST,
+  check: {
+    http: 'http://' + HOST + ':' + PORT + '/api/v1/health',
+    interval: '1s'
+  }
+});
 
 
 app.get('/api/v1/ip', function (req, res) {
-  res.send({version: 0.1, kind: TAGS, last_ip: '0.0.0.0', time: 'foo'});
+  res.send({version: 2.1, kind: TAGS, last_ip: '0.0.0.0', time: 'foo'});
 });
 
 app.get('/api/v1/health', function (req, res) {
   res.send({health: true});
 });
 
-app.listen(PORT);
+var server = app.listen(PORT);
 
-
-process.on('SIGTERM',function() {
-  console.log('DEREGISTER!!!!');
-  process.exit(1);
+process.on('SIGTERM', function() {
+  consul.agent.service.deregister('worker-' + HOST, function (a,b,c) {
+    setTimeout(function() {
+      process.exit(0);
+    }, 5000);
+  });
 });
-
