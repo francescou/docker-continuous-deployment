6d119580f5f30a23028e7eb4de6843b2f2a0c0cd
Francesco Uliana <francesco@uliana.it>
2015-12-20 11:08:45 +0100

    refactoring

--- rest-ip/app.js
+++ rest-ip/app.js
@@ -3,42 +3,15 @@
 var consul = require('consul')({host: 'consul'});
 var express = require('express');
 var _ = require('lodash');
+var os = require('os');
 
 var app = express();
 
 var PORT = 3000;
-
-var os = require('os');
-var ifaces = os.networkInterfaces();
-
-var x = _.filter(ifaces, function (o, item) {
-  return item !== 'lo';
-});
-
-var y = _.filter(x[0], function (el) {
-  return el.family === 'IPv4';
-});
-
-var HOST = y[0].address;
-
 var TAGS = process.env.TAGS;
-var tags = TAGS.split(',');
-
-consul.agent.service.register({
-  tags: tags,
-  name: 'rest-ip',
-  address: HOST,
-  port: PORT,
-  id: 'worker-' + HOST,
-  check: {
-    http: 'http://' + HOST + ':' + PORT + '/api/v1/health',
-    interval: '1s'
-  }
-});
-
 
 app.get('/api/v1/ip', function (req, res) {
-  res.send({version: 2.1, kind: TAGS, last_ip: '0.0.0.0', time: 'foo'});
+  res.send({version: 0.1, kind: TAGS, last_ip: '0.0.0.0', time: 'foo'});
 });
 
 app.get('/api/v1/health', function (req, res) {
@@ -47,10 +20,32 @@ app.get('/api/v1/health', function (req, res) {
 
 var server = app.listen(PORT);
 
-process.on('SIGTERM', function() {
-  consul.agent.service.deregister('worker-' + HOST, function (a,b,c) {
-    setTimeout(function() {
-      process.exit(0);
-    }, 5000);
+require('dns').lookup(os.hostname(), function (err, add) {
+  console.log('addr: '+add);
+  consul.agent.service.register({
+    tags: TAGS.split(','),
+    name: 'rest-ip',
+    address: add,
+    port: PORT,
+    id: 'worker-' + add,
+    check: {
+      http: 'http://' + add + ':' + PORT + '/api/v1/health',
+      interval: '1s'
+    }
+  }, function () {
+    console.log('registration done');
+    process.on('SIGTERM', function() {
+    console.log('deregistering');
+      consul.agent.service.deregister('worker-' + add, function () {
+        console.log('deregistered');
+        setTimeout(function() {
+          console.log('shutting down server');
+          server.close();
+        }, 5000);
+      });
+    });
   });
+
 });
+
+
